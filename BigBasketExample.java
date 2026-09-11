846046fbd8074922abdd4d2cff975fdd3db6fb8a
Indu-H <167083243+Indu-H@users.noreply.github.com>
2026-09-11 09:31:42 +0530

    Update BigBasketExample.java

--- BigBasketExample.java
+++ BigBasketExample.java
@@ -1,4 +1,4 @@
-ackage Demo;
+package Demo;
 
 import java.time.Duration;
 import java.util.List;
@@ -17,6 +17,22 @@ public class BigBasketExample {
 		driver.get("https://www.bigbasket.com/pb/boss/");
 		driver.findElement(By.xpath("(//input[@placeholder='Search for Products...'])[2]")).sendKeys("pineapple");
 		Thread.sleep(2000);
-		driver.findElement(By.xpath("//span[text()=' - Organically Grown']")).click();
+		driver.findElement(By.xpath("//span[text()=' - Organically Grown']"));
 		Thread.sleep(2000);
-		driver.findElement(By.xpath("(//button[text()='Add' ]/ancestor::li[@class='sc-cXPBUD gVNfkF'])[3]")).click();
+		driver.findElement(By.xpath("(//button[text()='Add'])[4]")).click();
+		WebElement text1 = driver.findElement(By.xpath("//p[text()='Item has been added to your basket successfully']"));
+		
+		//verify
+		if(text1.isDisplayed())
+		{
+			System.out.println("product is added to cart sucessfully");
+	
+		}
+		else
+		{
+			System.out.println("product not added to cart");
+			
+		}
+	}
+}
+
