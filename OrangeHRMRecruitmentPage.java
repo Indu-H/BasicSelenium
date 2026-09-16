6d35bc0e3b40b638668224efcc0bf5baff6c64c8
Indu-H <167083243+Indu-H@users.noreply.github.com>
2026-09-16 13:49:04 +0530

    Update OrangeHRMRecruitmentPage.java

--- OrangeHRMRecruitmentPage.java
+++ OrangeHRMRecruitmentPage.java
@@ -1,236 +1,157 @@
 package PomUtilities;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class OrangeHRMRecruitmentPage {
 
     WebDriver driver;
+    WebDriverWait wait;
 
     public OrangeHRMRecruitmentPage(WebDriver driver) {
-
         this.driver = driver;
-
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(20));
         PageFactory.initElements(driver, this);
     }
 
-
-    
     // ADD BUTTON
-
-    @FindBy(xpath = "//button[text()=' Add ']")
+    @FindBy(xpath = "//button[normalize-space()='Add']")
     private WebElement addButton;
 
-
-    
     // FIRST NAME
-
     @FindBy(name = "firstName")
     private WebElement firstName;
 
-
-    
     // MIDDLE NAME
-   
-
     @FindBy(name = "middleName")
     private WebElement middleName;
 
-
-    
     // LAST NAME
-    
-
     @FindBy(name = "lastName")
     private WebElement lastName;
 
-
-    // =====================================================
     // EMAIL
-    // =====================================================
-
-    @FindBy(xpath = "//label[contains(normalize-space(),'Email')]/ancestor::div[contains(@class,'oxd-input-group')]//input")
+    @FindBy(xpath = "//label[normalize-space()='Email']/ancestor::div[contains(@class,'oxd-input-group')]//input")
     private WebElement email;
 
-
-    // =====================================================
     // CONTACT NUMBER
-    // =====================================================
-
     @FindBy(xpath = "//label[normalize-space()='Contact Number']/ancestor::div[contains(@class,'oxd-input-group')]//input")
     private WebElement contactNumber;
 
-
-    // =====================================================
     // RESUME
-    // =====================================================
-
     @FindBy(xpath = "//input[@type='file']")
     private WebElement resume;
 
-
-    // =====================================================
-    // DATE OF APPLICATION
-    // =====================================================
-
-    @FindBy(xpath = "//input[@placeholder='yyyy-dd-mm']")
-    private WebElement dateOfApplication;
-
-
-    // =====================================================
     // SAVE BUTTON
-    // =====================================================
-
-    @FindBy(xpath = "//button[text()=' Save ']")
+    @FindBy(xpath = "//button[normalize-space()='Save']")
     private WebElement saveButton;
 
-
-    // =====================================================
-    // CANDIDATES BUTTON
-    // =====================================================
-
-    @FindBy(xpath = "//a[text()='Candidates']")
+    // CANDIDATES TAB
+    @FindBy(xpath = "//a[normalize-space()='Candidates']")
     private WebElement candidates;
 
 
-    // =====================================================
-    // ADD BUTTON
-    // =====================================================
+    // ADD
+    public void clickAdd() {
 
-    public void getAddButton() {
-
+        wait.until(ExpectedConditions.elementToBeClickable(addButton));
         addButton.click();
     }
 
 
-    // =====================================================
     // FIRST NAME
-    // =====================================================
+    public void enterFirstName(String value) {
 
-    public void getFirstName(String value) {
-
+        wait.until(ExpectedConditions.visibilityOf(firstName));
         firstName.sendKeys(value);
     }
 
 
-    // =====================================================
     // MIDDLE NAME
-    // =====================================================
+    public void enterMiddleName(String value) {
 
-    public void getMiddleName(String value) {
-
+        wait.until(ExpectedConditions.visibilityOf(middleName));
         middleName.sendKeys(value);
     }
 
 
-    // =====================================================
     // LAST NAME
-    // =====================================================
+    public void enterLastName(String value) {
 
-    public void getLastName(String value) {
-
+        wait.until(ExpectedConditions.visibilityOf(lastName));
         lastName.sendKeys(value);
     }
 
 
-    // =====================================================
     // VACANCY
-    // =====================================================
+    public void selectVacancy(String value) {
 
-    public void getVacancy(String value) {
-
-        WebElement vacancyDropdown = driver.findElement(
-                By.xpath(
-                        "//label[normalize-space()='Vacancy']" +
-                        "/ancestor::div[contains(@class,'oxd-input-group')]" +
-                        "//div[contains(@class,'oxd-select-text')]"
-                )
+        By vacancyDropdownLocator = By.xpath(
+            "//label[normalize-space()='Vacancy']" +
+            "/ancestor::div[contains(@class,'oxd-input-group')]" +
+            "//div[contains(@class,'oxd-select-text')]"
         );
+
+        WebElement vacancyDropdown =
+            wait.until(ExpectedConditions.elementToBeClickable(vacancyDropdownLocator));
 
         vacancyDropdown.click();
 
-        WebElement vacancyOption = driver.findElement(
-                By.xpath(
-                        "//div[@role='listbox']//span[normalize-space()='" +
-                        value +
-                        "']"
-                )
+        By vacancyOptionLocator = By.xpath(
+            "//div[contains(@class,'oxd-select-option')]" +
+            "//*[normalize-space()='" + value + "']"
         );
+
+        WebElement vacancyOption =
+            wait.until(ExpectedConditions.elementToBeClickable(vacancyOptionLocator));
 
         vacancyOption.click();
     }
 
 
-    // =====================================================
     // EMAIL
-    // =====================================================
+    public void enterEmail(String value) {
 
-    public void getEmail(String value) {
-
+        wait.until(ExpectedConditions.visibilityOf(email));
         email.sendKeys(value);
     }
 
 
-    // =====================================================
     // CONTACT NUMBER
-    // =====================================================
+    public void enterContactNumber(String value) {
 
-    public void getContactNumber(String value) {
-
+        wait.until(ExpectedConditions.visibilityOf(contactNumber));
         contactNumber.sendKeys(value);
     }
 
 
-    // =====================================================
     // RESUME
-    // =====================================================
-
     public void getResume(String path) {
 
+        // File input is hidden, so don't use visibilityOf()
         resume.sendKeys(path);
     }
 
 
-    
-    // DATE OF APPLICATION
-    
-
-    public void getDateOfApplication(String value)
-    {
-        WebElement applicationDate = driver.findElement(
-                By.xpath("//label[normalize-space()='Date of Application']/ancestor::div[contains(@class,'oxd-input-group')]//input")
-        );
-
-        applicationDate.click();
-
-        applicationDate.sendKeys(Keys.CONTROL, "a");
-
-        applicationDate.sendKeys(Keys.BACK_SPACE);
-
-        applicationDate.sendKeys(value);
-
-        applicationDate.sendKeys(Keys.TAB);
-    }
-    
     // SAVE
-
-
     public void getSaveButton() {
 
+        wait.until(ExpectedConditions.elementToBeClickable(saveButton));
         saveButton.click();
     }
 
 
-    
     // CANDIDATES
-    
+    public void clickCandidates() {
 
-    public void getCandidates() {
-
+        wait.until(ExpectedConditions.elementToBeClickable(candidates));
         candidates.click();
     }
 }
