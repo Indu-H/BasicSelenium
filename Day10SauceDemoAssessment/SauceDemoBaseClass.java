99ba70f845ec2af1c5e3de30f5d5e37d3b045d7d
Indu-H <167083243+Indu-H@users.noreply.github.com>
2026-09-17 20:10:29 +0530

    Create SauceDemoBaseClass.java

---
+++ Day10SauceDemoAssessment/SauceDemoBaseClass.java
@@ -0,0 +1,66 @@
+package BaseClassUtility;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+public class SauceDemoBaseClass {
+
+    protected WebDriver driver;
+    protected Properties properties;
+
+    @BeforeClass
+    public void setUp() throws IOException {
+
+        // CREATE PROPERTIES OBJECT
+        properties = new Properties();
+
+        // OPEN PROPERTY FILE
+        FileInputStream propertyFile = new FileInputStream(
+            "./src/test/resources/DDT/SauceDemo.properties"
+        );
+
+        // LOAD PROPERTY FILE
+        properties.load(propertyFile);
+
+        // CLOSE PROPERTY FILE
+        propertyFile.close();
+
+        // CHROME SETTINGS
+        ChromeOptions settings = new ChromeOptions();
+
+        // LAUNCH BROWSER
+        driver = new ChromeDriver(settings);
+
+        // MAXIMIZE BROWSER
+        driver.manage().window().maximize();
+
+        // IMPLICIT WAIT
+        driver.manage().timeouts().implicitlyWait(
+            Duration.ofSeconds(10)
+        );
+
+        System.out.println("Browser launched.");
+    }
+
+
+    @AfterClass
+    public void tearDown() {
+
+        if (driver != null) {
+
+            driver.quit();
+
+            System.out.println("Browser closed.");
+        }
+    }
+}
+
+
