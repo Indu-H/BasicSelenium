dcd9243d10398eb76f7c817125e60936c59a495e
Indu-H <167083243+Indu-H@users.noreply.github.com>
2026-09-18 10:57:06 +0530

    Update SauceDemoBaseClass.java

--- Day10SauceDemoAssessment/SauceDemoBaseClass.java
+++ Day10SauceDemoAssessment/SauceDemoBaseClass.java
@@ -1,66 +1,134 @@
+
 package BaseClassUtility;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
+
 public class SauceDemoBaseClass {
 
+
     protected WebDriver driver;
+
     protected Properties properties;
 
+
+
     @BeforeClass
+
     public void setUp() throws IOException {
 
+
         // CREATE PROPERTIES OBJECT
+
         properties = new Properties();
 
+
+
         // OPEN PROPERTY FILE
-        FileInputStream propertyFile = new FileInputStream(
-            "./src/test/resources/DDT/SauceDemo.properties"
-        );
+
+        FileInputStream propertyFile =
+                new FileInputStream(
+                        "./src/test/resources/DDT/SauceDemo.properties"
+                );
+
+
 
         // LOAD PROPERTY FILE
+
         properties.load(propertyFile);
 
+
+
         // CLOSE PROPERTY FILE
+
         propertyFile.close();
 
+
+
         // CHROME SETTINGS
-        ChromeOptions settings = new ChromeOptions();
 
-        // LAUNCH BROWSER
-        driver = new ChromeDriver(settings);
+        ChromeOptions settings =
+                new ChromeOptions();
 
-        // MAXIMIZE BROWSER
-        driver.manage().window().maximize();
 
-        // IMPLICIT WAIT
-        driver.manage().timeouts().implicitlyWait(
-            Duration.ofSeconds(10)
+
+        // AVOID CHANGE PASSWORD POPUP
+
+        Map<String, Object> prefs =
+                new HashMap<>();
+
+
+        prefs.put(
+                "profile.password_manager_leak_detection",
+                false
         );
 
-        System.out.println("Browser launched.");
+
+        settings.setExperimentalOption(
+                "prefs",
+                prefs
+        );
+
+
+
+        // LAUNCH BROWSER
+
+        driver =
+                new ChromeDriver(settings);
+
+
+
+        // MAXIMIZE BROWSER
+
+        driver.manage().window().maximize();
+
+
+
+        // IMPLICIT WAIT
+
+        driver.manage().timeouts().implicitlyWait(
+                Duration.ofSeconds(10)
+        );
+
+
+
+        System.out.println(
+                "Browser launched."
+        );
+
     }
+
 
 
     @AfterClass
-    public void tearDown() {
+
+    public void closeBrowser() {
+
 
         if (driver != null) {
 
+
             driver.quit();
 
-            System.out.println("Browser closed.");
+
+            System.out.println(
+                    "Browser closed."
+            );
+
         }
+
     }
+
 }
-
-
